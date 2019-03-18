9c80697eec1e545299ceb7d9fd1b3410359350f8
Samuel <js.rosemond@gmail.com>
2019-03-18 00:58:35 -0400

    merge

--- grip-app/src/components/OtherPages/BeveragesPage.js
+++ grip-app/src/components/OtherPages/BeveragesPage.js
@@ -8,21 +8,11 @@ export default function Beverages(props) {
 			{value => {
 				return (
 					<Fragment>
-						<NewPage menu={value.drinks} location={props.location.pathname} />
-
-						<NewPage
-							menu={value.beer}
-							location={props.location.pathname}
-							display={value.sapporo_beer_image}
-						/>
-
 						<NewPage
 							menu={value.house_whine}
 							location={props.location.pathname}
 							display={value.whine_image}
 						/>
-						<NewPage menu={value.white_whine} location={props.location.pathname} headline={'White Whine'} />
-						<NewPage menu={value.red_whine} location={props.location.pathname} headline={'Red Whine'} />
 					</Fragment>
 				);
 			}}
